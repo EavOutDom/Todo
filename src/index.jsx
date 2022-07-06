6ce05b143c140f316ada2898b483b4df87041fc4
EavOutDom <eavoutdom400@gmail.com>
2022-07-06 15:07:17 +0700

    update persist state

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,16 @@ import "./index.css";
 import App from "./components/app";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistStore} from 'redux-persist';
+let persistor = persistStore(store)
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+       <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
     </React.StrictMode>
 );
--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,31 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todo/todoSlice";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todoReducer)
+
 export const store = configureStore({
-    reducer: {
-        todo: todoReducer,
-    },
-});
+    reducer: {todo: persistedReducer},
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  })
